4275f9bd7cffbd5c20722556a596462f125110a3
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2017-08-12 16:58:09 +0000

    Create Test trait
    - Replaces the Future trait for test implementations
    - Create a IntoTest trait
    - Update TestSpawner to have an TestSetup associated type
    - Update TestScheduler to only need a TestSpawner generic type
    - Add a TestResult::from_poll helper method

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,9 @@ extern crate futures;
 mod test_result;
 mod test_scheduler;
 mod test_spawner;
+mod test;
 
 pub use test_result::TestResult;
 pub use test_scheduler::TestScheduler;
 pub use test_spawner::TestSpawner;
+pub use test::Test;
