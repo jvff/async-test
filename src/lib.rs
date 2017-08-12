5556a9dbb4fd6edf8cff8d8c19f6ecb1caf0ce71
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2017-08-12 15:21:20 +0000

    Create TestSpawner trait
    - Use it instead of a generic FnOnce to spawn tests

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@ extern crate futures;
 
 mod test_result;
 mod test_scheduler;
+mod test_spawner;
 
 pub use test_result::TestResult;
 pub use test_scheduler::TestScheduler;
+pub use test_spawner::TestSpawner;
