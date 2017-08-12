674c18e72457ea2f1cf8197320c119d81f8c33e9
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2017-08-12 17:48:30 +0000

    Create TestReporter structure
    - Use termion crate to print test results with color output

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,13 @@
 extern crate futures;
+extern crate termion;
 
+mod test_reporter;
 mod test_result;
 mod test_scheduler;
 mod test_spawner;
 mod test;
 
+pub use test_reporter::TestReporter;
 pub use test_result::TestResult;
 pub use test_scheduler::TestScheduler;
 pub use test_spawner::TestSpawner;
