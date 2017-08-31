f5320301f1f22bed47de47e18a73c74109163d47
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2017-08-31 12:37:37 +0000

    Create SequentialTestScheduler structure
    - Only starts a new test after the previous one has completed

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,14 @@ extern crate futures;
 extern crate termion;
 
 mod parallel_test_scheduler;
+mod sequential_test_scheduler;
 mod test_reporter;
 mod test_result;
 mod test_spawner;
 mod test;
 
 pub use parallel_test_scheduler::ParallelTestScheduler;
+pub use sequential_test_scheduler::SequentialTestScheduler;
 pub use test_reporter::TestReporter;
 pub use test_result::TestResult;
 pub use test_spawner::TestSpawner;
