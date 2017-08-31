740f0cfdf2f63de9b0995ae12e6f361c3bc632a2
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2017-08-31 12:11:25 +0000

    Rename TestScheduler into ParallelTestScheduler

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,14 @@
 extern crate futures;
 extern crate termion;
 
+mod parallel_test_scheduler;
 mod test_reporter;
 mod test_result;
-mod test_scheduler;
 mod test_spawner;
 mod test;
 
+pub use parallel_test_scheduler::ParallelTestScheduler;
 pub use test_reporter::TestReporter;
 pub use test_result::TestResult;
-pub use test_scheduler::TestScheduler;
 pub use test_spawner::TestSpawner;
 pub use test::Test;
