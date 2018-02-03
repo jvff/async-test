14ee96444c33d8ad49e9ed5ccb0f7e84d01163cd
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2018-02-03 21:14:13 +0000

    Make EmptyTestFixture more generic

--- src/empty_test_fixture.rs
+++ src/empty_test_fixture.rs
@@ -3,32 +3,37 @@ use std::marker::PhantomData;
 use super::test::Test;
 use super::test_fixture::TestFixture;
 
-pub struct EmptyTestFixture<T>
+pub struct EmptyTestFixture<S, T>
 where
+    S: Into<T>,
     T: Test,
 {
+    _test_spec_type: PhantomData<S>,
     _test_type: PhantomData<T>,
 }
 
-impl<T> EmptyTestFixture<T>
+impl<S, T> EmptyTestFixture<S, T>
 where
+    S: Into<T>,
     T: Test,
 {
     pub fn new() -> Self {
         EmptyTestFixture {
+            _test_spec_type: PhantomData,
             _test_type: PhantomData,
         }
     }
 }
 
-impl<T> TestFixture for EmptyTestFixture<T>
+impl<S, T> TestFixture for EmptyTestFixture<S, T>
 where
+    S: Into<T>,
     T: Test,
 {
-    type TestSpec = T;
+    type TestSpec = S;
     type Test = T;
 
-    fn start(&mut self, test: Self::TestSpec) -> Self::Test {
-        test
+    fn start(&mut self, test_spec: Self::TestSpec) -> Self::Test {
+        test_spec.into()
     }
 }
